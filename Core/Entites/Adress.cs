9588ccd3b4acceb87f518645778bab41ef5f68cb
saif <Saif@tacticmi.com>
2025-07-21 14:39:59 +0300

    Add project files.

---
+++ Core/Entites/Adress.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entites
+{
+    public class Adress : EntityBase
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public int Number { get; set; }
+    }
+}
